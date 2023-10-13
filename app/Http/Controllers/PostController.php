b79ef942c0e2d024d1f5fe8698ef6502331c50b8
honda628 <ec2-user@ip-172-31-17-1.ap-northeast-1.compute.internal>
2023-10-14 00:18:49 +0900

    8-2

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,6 +9,6 @@ class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return $post->get();
+  return view('post.index')->with(['posts' => $post->getPaginateBylimit(1)]);
     }
 }
