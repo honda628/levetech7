ef873223a4994bd9ab4312e87034339f97c73089
honda628 <ec2-user@ip-172-31-17-1.ap-northeast-1.compute.internal>
2023-10-15 19:03:26 +0900

    8-3

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,6 +9,16 @@ class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return $post->get();
+        return view('posts/index')->with(['posts' => $post->getPaginateByLimit()]);
+    }
+    
+    public function show(Post $post)
+    {
+      return view('posts/show')->with(['post' => $post]);
+    }
+    
+        public function getPaginateByLimit(int $limit_count = 10)
+        {
+            return $this->orderBY('updated_at', 'DESC')->lomit($limit_count)->get();
     }
 }
