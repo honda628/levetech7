8dbec2ef65274d0db5ef21b6409f2e414d4077ca
honda628 <ec2-user@ip-172-31-17-1.ap-northeast-1.compute.internal>
2023-10-21 05:16:19 +0900

    8-4

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -12,6 +12,11 @@ class Post extends Model
     public function getPaginateByLimit(int $limit_count = 10)
 {
     return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
+}
 
-}    
+protected $fillable = [
+    'title',
+    'body',
+    ];
+    
 }
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,11 @@ use App\Http\Controllers\PostController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/posts',[PostController::class,'index']);
-
+Route::get('/',[PostController::class,'index']);
+Route::get('/posts/create', [PostController::class, 'create']);
 Route::get('/posts/{post}', [PostController::class, 'show']);
+Route::post('/posts', [PostController::class, 'store']);
